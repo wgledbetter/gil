5e07d57f41fb11672ae3e57feaa2a17590efabfd
William G Ledbetter <williamgledbetter@gmail.com>
2021-03-21 21:01:33 +0000

    for pybind issue

--- main.cpp
+++ main.cpp
@@ -1,29 +1,5 @@
 #include <pybind11/pybind11.h>
 
-int greatest_common_divisor(int x, int y) {
-  int r, tmp;
-  if (x < y) {
-    tmp = x;
-    x = y;
-    y = tmp;
-  }
+int f1(int y) { return y; }
 
-  r = x % y;
-  while (r != 0) {
-    x = y;
-    y = r;
-    r = x % y;
-  }
-  return y;
-}
-
-// int least_common_multiple(int x, int y) {
-//   return x * y / greatest_common_divisor(x, y);
-// }
-
-PYBIND11_MODULE(gil, m) {
-  // m.doc() = "pybind11 example";
-  m.def("greatest_common_divisor", &greatest_common_divisor,
-        "A function which calculate greatest common divisor");
-  // m.def("least_common_multiple", &least_common_multiple, "");
-}
+PYBIND11_MODULE(gil, m) { m.def("f1", &f1); }
